1ba743ff440e117fe93f974426b81b561dba5a44
Niamh <niamh.linguist@gmail.com>
2024-12-02 20:58:29 +0200

    passing tests

--- tests/main-flow.spec.ts
+++ tests/main-flow.spec.ts
@@ -1,24 +1,40 @@
 import { test, expect } from '@playwright/test';
 
-const serviceURL = 'http://localhost:3000';
+const serviceURL = 'http://localhost:3000/';
+const routeToMock = '**/api/loan-calc?amount=*&period=*'
 
 test('main flow', async ({ page }) => {
-  await page.goto(serviceURL);
-  await page.getByTestId('id-small-loan-calculator-field-apply').click();
-  await page.getByTestId('login-popup-username-input').click();
-  await page.getByTestId('login-popup-username-input').fill('usern');
-  await page.getByTestId('login-popup-username-input').press('Tab');
-  await page.getByTestId('login-popup-password-input').fill('pwd');
-  await page.getByTestId('login-popup-continue-button').click();
-  await page.getByTestId('final-page-continue-button').click();
-  await page.getByTestId('final-page-success-ok-button').click();
+  await page.route(routeToMock, async (route) => {
+
+    await route.fulfill({
+      status: 400,
+    });
+  });
+
+  await page.goto(serviceURL)
+  await expect(page.getByTestId('id-small-loan-calculator-field-error')).toBeVisible()
 });
 
-test('redirect flow', async ({ page, request }) => {
-  await page.goto(serviceURL);
-  await page.getByTestId('id-image-element-button-image-1').click();
-  await expect( page.getByTestId('id-small-loan-calculator-field-apply') ).toBeInViewport()
-  await page.getByTestId('id-image-element-button-image-2').click();
-  await expect( page.getByTestId('id-small-loan-calculator-field-apply') ).toBeInViewport()
-})
+test.only('positive scenario', async ({ page }) => {
 
+  const valueMock = 42.8
+
+  await page.route(routeToMock, async (route) => {
+    const mockResponse = {
+      paymentAmountMonthly: valueMock,
+    };
+
+    await route.fulfill({
+      status: 200,
+      body: JSON.stringify(mockResponse),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  });
+
+  await page.goto(serviceURL)
+  await page.waitForResponse(routeToMock)
+  const amount = await page.getByTestId('ib-small-loan-calculator-field-monthlyPayment').textContent()
+  expect(amount).toBe(`${valueMock} €`)
+});
